609929d89cdab9521ee946fed252a6916e856af6
Visal06 <khmerus07@gmail.com>
2024-08-24 11:04:19 +0700

    Lasted update

--- mycourse_flutter/lib/api/api_service.dart
+++
@@ -1,6 +0,0 @@
-const String base = "http://localhost:8000/api";
-
-class ApiService {
-  static const String loginapi = "$base/login";
-  static const String registerapi = "$base/register";
-}
--- mycourse_flutter/lib/screen/auth/logic/loginview.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mycourse_flutter/model/userresponse.dart';
-
-abstract class Loginview {
-  void onLoading(bool loading);
-  void onLogin();
-  void onString(String str);
-  void onSuccess(Userresponse user);
-}
